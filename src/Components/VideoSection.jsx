b1fbc2372857d1de43ddb3e2352599bd90ac1d36
Atrix-team <Atrixteams@example.com>
2025-03-05 22:30:08 +0530

    six commit

--- src/Components/VideoSection.jsx
+++ src/Components/VideoSection.jsx
@@ -2,17 +2,16 @@ import React from 'react';
 
 const VideoSection = () => {
   return (
-    <div className="pointer-events-none">
-      <iframe
-        className="w-full aspect-video"
-        src="https://www.youtube.com/embed/zWZMN9DGuj4?autoplay=1&mute=1&controls=0&loop=1&playlist=zWZMN9DGuj4&start=2"
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerPolicy="strict-origin-when-cross-origin"
-        allowFullScreen
-      />
-    </div>
+    <div>
+    <iframe 
+        class="w-full aspect-video pointer-events-none" 
+        src="https://www.youtube.com/embed/zWZMN9DGuj4?si=XpIZ5znWpzxguDbo&start=2&controls=0&showinfo=0&modestbranding=1&autoplay=1&loop=1&mute=1&playlist=zWZMN9DGuj4" 
+        frameborder="0" 
+        allow="autoplay; encrypted-media" 
+        allowfullscreen>
+    </iframe>
+</div>
+
   );
 };
 
