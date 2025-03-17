7bddee8e9215f57e84d9d4e9c7fcdd446196b7d5
Atrix-team <Atrixteams@gmail.com>
2025-03-17 21:23:00 +0530

    sdfd

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { OutLet } from './OutLet/OutLet';
 import Home from './Pages/Home';
-import About from './Pages/About';
+
 
 import AppLayout from "./Dashboard/layout/AppLayout"
 import DashboardHome from "./Dashboard/pages/DashboardHome/HomePage";
@@ -10,6 +10,7 @@ import Technology from "./Dashboard/pages/TechnologyPage/Technology"
 import NewAddTechnology from "./Dashboard/pages/TechnologyPage/NewAddTechnology";
 import CategoryTechnology from "./Dashboard/pages/TechnologyPage/categoryTechnology";
 import SignInForm from "./Dashboard/components/auth/SignInForm"
+import AboutUs from './Pages/About/AboutUs';
 
 
 
@@ -21,7 +22,7 @@ export default function App() {
             <Routes>
                   <Route path="/" element={<OutLet />}>
                     <Route index element={<Home />} />
-                    <Route path='/about' element={<About />} />
+                    <Route path='/AboutUs' element={<AboutUs />} />
 
                   </Route>
 
