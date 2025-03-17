8aa7701b59e7e25cf6999253836c1db259e6ecba
Anil Verma <72238305+Anilkv9883@users.noreply.github.com>
2025-03-17 21:20:33 +0530

    ss

---
+++ src/Pages/About.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const About = () => {
+
+
+  return (
+    <>
+
+      <div className="container">
+        <div className="row">
+          <div className="col"> </div>
+          <div className="col"></div>
+          <div className="col"></div>
+        </div>
+      </div>
+      <div className="container">
+        <h1>About Us</h1>
+      </div>
+
+
+    </>
+
+  )
+}
+
+export default About
